f695d5465cb2b895bf88b9a77bb2efcdcdd53a1b
Seth Davila <sdavil15@stu.southtexascollege.edu>
2025-02-15 22:16:34 -0600

    importing and finishing up the project

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
-<script setup>
-
-import TaskList from './components/TaskList.vue';
+<script>
 import TaskForm from './components/TaskForm.vue';
+import TaskList from './components/TaskList.vue';
+import TaskStats from './components/TaskStats.vue';
 
+export default {
+components: {
+    TaskForm,
+    TaskList,
+    TaskStats
+  },
+  data() {
+    return {
+      tasks: []
+    };
+  },
+  methods: {
+  addTask(task) {
+      this.tasks.push(task);
+    },
+  toggleCompletion(taskId) {
+      const task = this.tasks.find(t => t.id === taskId);
+      if (task) {
+        task.completed = !task.completed;
+      }
+    },
+  deleteTask(taskId) {
+      this.tasks = this.tasks.filter(t => t.id !== taskId);
+    }
+  }
+};
 </script>
 
 
 <template>
-
-
-
+  <div>
+    <h1>Welcome to the Task Tracker</h1>
+    <TaskForm @add-task="addTask" />
+    <TaskList :tasks="tasks" @toggle-completion="toggleCompletion" @delete-task="deleteTask" />
+    <TaskStats :tasks="tasks" />
+  </div>
 </template>
