c49cbd6cec1c3f647d9203b7d5076003868f3f2b
Seth Davila <sdavil15@stu.southtexascollege.edu>
2025-02-15 21:59:50 -0600

    Creation of TaskList

---
+++ src/components/TaskList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h2>Your Tasks</h2>
+      <ul>
+        <li v-for="task in tasks" :key="task.id">
+          <span :class="{'line-through': task.completed}">{{ task.title }}</span>
+          <button @click="toggleCompletion(task)">Mark as {{ task.completed ? 'Incomplete' : 'Completed' }}</button>
+          <button @click="deleteTask(task.id)">Delete</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['tasks'],
+    methods: {
+      toggleCompletion(task) {
+        this.$emit('toggle-completion', task.id);
+      },
+      deleteTask(taskId) {
+        this.$emit('delete-task', taskId);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  button {
+    margin-left: 10px;
+  }
+  .line-through {
+    text-decoration: line-through;
+  }
+  </style>
