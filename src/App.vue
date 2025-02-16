76ac04f3eff5d02288a3e7cdd41be1d4795d2620
Seth Davila <sdavil15@stu.southtexascollege.edu>
2025-02-15 22:10:20 -0600

    Creation of TaskStatus

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,13 @@
+<script setup>
 
+import TaskList from './components/TaskList.vue';
+import TaskForm from './components/TaskForm.vue';
+
+</script>
+
+
+<template>
+
+
+
+</template>
---
+++ src/components/TaskStatus.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  props: ['tasks'],
+  computed: {
+    totalTasks() {
+      return this.tasks.length;
+    },
+    completedTasks() {
+      return this.tasks.filter(task => task.completed).length;
+    },
+    completionRate() {
+      return this.totalTasks > 0 ? ((this.completedTasks / this.totalTasks) * 100).toFixed(2) : 0;
+    }
+  }
+};
+</script>
+
+<template>
+    <div>
+      <h2>Task Stats</h2>
+      <p>Total Tasks: {{ totalTasks }}</p>
+      <p>Completed Tasks: {{ completedTasks }}</p>
+      <p>Completion Rate: {{ completionRate }}%</p>
+    </div>
+  </template>
+
+<style scoped>
+p {
+  margin: 5px 0;
+}
+</style>
